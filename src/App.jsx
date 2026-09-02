b68da388194e14437a918e5f9c2474c2994d8cec
oslandamiral-bit <oslandamiral@gmail.com>
2026-09-02 14:04:27 +0100

    feat: point d'entrée React et composant racine App

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
+import Skills from './components/Skills.jsx'
+import Projects from './components/Projects.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+  return (
+    <div className="bg-dark text-light min-h-screen">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
